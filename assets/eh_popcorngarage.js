357b77f1d302bf7f77cd6aeed2eda059f89f200c
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2022-01-05 16:27:01 +0100

    game solution ok

--- assets/eh_popcorngarage.js
+++ assets/eh_popcorngarage.js
@@ -1,74 +1,132 @@
+
+const wordsvalidated = [];
+
+
+// function time event text
 function timeEvent (){
   setTimeout(() => {
     $('.eventtext').empty();
   },10000);
 }
 
+
+
+// function garage
 $('#formgarage').on('submit', function(event) {
     
+
   event.preventDefault();
     const words = $('#formgarage #userinput').val(); 
     console.log(words);
-      if (words === "KLIMT") {
-        $('#result').text("Well, good answer!");
-        timeEvent();
+
+      if (words === "KLIMT" && !(wordsvalidated.includes('KLIMT'))) {
+        $('#result').text("Good answer!");
+        timeEvent()
+        const count = wordsvalidated.push('KLIMT')
+        console.log(count)
+        console.log(wordsvalidated);
     }
-        else if (words === "VAN GOGH") {
-          $('#result').text("Well, good answer!");
-          timeEvent();
+        else if (words === "VAN GOGH" && !(wordsvalidated.includes('VAN GOGH'))) {
+          $('#result').text("Good answer!");
+          timeEvent()
+          const count = wordsvalidated.push('VAN GOGH')
+          console.log(count)
+          console.log(wordsvalidated);
       }
-          else if (words === "BANKSY") {
-            $('#result').text("Well, good answer!");
-            timeEvent();
+          else if (words === "BANKSY" && !(wordsvalidated.includes('BANKSY'))) {
+            $('#result').text("Good answer!");
+            timeEvent()
+            const count = wordsvalidated.push('BANKSY')
+            console.log(count)
+            console.log(wordsvalidated);
         }
-            else if (words === "DINOS ALIVE"){
-              $('#result').text("Well, good answer!");
-              timeEvent();
+            else if (words === "DINOS ALIVE" && !(wordsvalidated.includes('DINOS ALIVE'))) {
+              $('#result').text("Good answer!");
+              timeEvent()
+              const count = wordsvalidated.push('DINOS ALIVE')
+              console.log(count)
+              console.log(wordsvalidated);
           }
-              else if (words === "MONET") {
-                $('#result').text("Well, good answer!");
-                timeEvent();
+              else if (words === "MONET" && !(wordsvalidated.includes('MONET'))) {
+                $('#result').text("Good answer!");
+                timeEvent()
+                const count = wordsvalidated.push('MONET')
+                console.log(count)
+                console.log(wordsvalidated);
             }
-                else if (words === "THE TERRACOTTA ARMY") {
-                  $('#result').text("Well, good answer!");
-                  timeEvent();
+                else if (words === "THE TERRACOTTA ARMY" && !(wordsvalidated.includes('THE TERRACOTTA ARMY'))) {
+                  $('#result').text("Good answer!");
+                  timeEvent()
+                  const count = wordsvalidated.push('THE TERRACOTTA ARMY')
+                  console.log(count)
+                  console.log(wordsvalidated);
               }
-                  else if (words === "ALICE IN WONDERLAND") {
-                    $('#result').text("Well, good answer!");
-                    timeEvent();
+                  else if (words === "ALICE IN WONDERLAND" && !(wordsvalidated.includes('ALICE IN WONDERLAND'))) {
+                    $('#result').text("Good answer!");
+                    timeEvent()
+                    const count = wordsvalidated.push('ALICE IN WONDERLAND')
+                    console.log(count)
+                    console.log(wordsvalidated);
                 }
-                    else if (words === "THE POWER OF POISON") {
-                      $('#result').text("Well, good answer!");
-                      timeEvent();
+                    else if (words === "THE POWER OF POISON" && !(wordsvalidated.includes('THE POWER OF POISON'))) {
+                      $('#result').text("Good answer!");
+                      timeEvent()
+                      const count = wordsvalidated.push('THE POWER OF POISON')
+                      console.log(count)
+                      console.log(wordsvalidated);
                   }
-                      else if (words === "THE ART OF THE BRICK") {
-                        $('#result').text("Well, good answer!");
-                        timeEvent();
+                      else if (words === "THE ART OF THE BRICK" && !(wordsvalidated.includes('THE ART OF THE BRICK'))) {
+                        $('#result').text("Good answer!");
+                        timeEvent()
+                        const count = wordsvalidated.push('THE ART OF THE BRICK')
+                        console.log(count)
+                        console.log(wordsvalidated);
                     }
-                        else if (words === "MOMENT WITH THE BUTTERFLIES") {
-                          $('#result').text("Well, good answer!");
-                          timeEvent();
+                        else if (words === "MOMENT WITH THE BUTTERFLIES" && !(wordsvalidated.includes('MOMENT WITH THE BUTTERFLIES'))) {
+                          $('#result').text("Good answer!");
+                          timeEvent()
+                          const count = wordsvalidated.push('MOMENT WITH THE BUTTERFLIES')
+                          console.log(count)
+                          console.log(wordsvalidated);
                       }
-                          else if (words === "MAGRITTE") {
-                            $('#result').text("Well, good answer!");
-                            timeEvent();
+                          else if (words === "MAGRITTE" && !(wordsvalidated.includes('MAGRITTE'))) {
+                            $('#result').text("Good answer!");
+                            timeEvent()
+                            const count = wordsvalidated.push('MAGRITTE')
+                            console.log(count)
+                            console.log(wordsvalidated);
                         }
-                            else if (words === "DALI") {
-                              $('#result').text("Well, good answer!");
-                              timeEvent();
+                            else if (words === "DALI" && !(wordsvalidated.includes('DALI'))) {
+                              $('#result').text("Good answer!");
+                              timeEvent()
+                              const count = wordsvalidated.push('DALI')
+                              console.log(count)
+                              console.log(wordsvalidated);
                           }
-                              else if (words === "TOUTANKHAMON") {
-                                $('#result').text("Well, good answer!");
-                                timeEvent();
+                              else if (words === "TOUTANKHAMON" && !(wordsvalidated.includes('TOUTANKHAMON'))) {
+                                $('#result').text("Good answer!");
+                                timeEvent()
+                                const count = wordsvalidated.push('TOUTANKHAMON')
+                                console.log(count)
+                                console.log(wordsvalidated);
                             }
-                                else if (words === "POP MASTER") {
-                                  $('#result').text("Well, good answer!");
-                                  timeEvent();
+                                else if (words === "POP MASTER" && !(wordsvalidated.includes('POP MASTER'))) {
+                                  $('#result').text("Good answer!");
+                                  timeEvent()
+                                  const count = wordsvalidated.push('POP MASTER')
+                                  console.log(count)
+                                  console.log(wordsvalidated);
                               }
+                                  else if (words === "INCA" && !(wordsvalidated.includes('INCA'))) {
+                                    $('#result').text("Good answer!");
+                                    timeEvent()
+                                    const count = wordsvalidated.push('POP MASTER')
+                                    console.log(count)
+                                    console.log(wordsvalidated);
+                                }
     else {
-      $('#result').text("Fail, try again!");
+      $('#result').text("Well, try again!");
       timeEvent();
   }
     });
-    
     
