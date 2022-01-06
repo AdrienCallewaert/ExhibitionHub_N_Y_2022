96e1445cfd9fab7299495ef6c8a7db15bf43c97b
Adrien Callewaert <76485285+AdrienCallewaert@users.noreply.github.com>
2022-01-06 21:57:48 +0100

    up

--- assets/eh_popcorngarage.js
+++ assets/eh_popcorngarage.js
@@ -12,6 +12,7 @@ function timeEvent() {
     }, 10000);
 }
 
+
 // function garage
 $('#formgarage').on('submit', function(event) {
 
@@ -28,7 +29,7 @@ $('#formgarage').on('submit', function(event) {
         console.log(wordsvalidated)
         countscreen();
     } else if (words === "VAN GOGH" && !(wordsvalidated.includes('VAN GOGH'))) {
-        $('#result').text("Van Gogh, you found it!");
+        $('#result').text("Vincent, you found it!");
         timeEvent()
         const count = wordsvalidated.push('VAN GOGH')
         console.log(count)
@@ -42,7 +43,7 @@ $('#formgarage').on('submit', function(event) {
         console.log(wordsvalidated)
         countscreen();
     } else if (words === "DINOS ALIVE" && !(wordsvalidated.includes('DINOS ALIVE'))) {
-        $('#result').text("The dinos, you found it!");
+        $('#result').text("Dinos, you found it!");
         timeEvent()
         const count = wordsvalidated.push('DINOS ALIVE')
         console.log(count)
@@ -56,7 +57,7 @@ $('#formgarage').on('submit', function(event) {
         console.log(wordsvalidated)
         countscreen();
     } else if (words === "THE TERRACOTTA ARMY" && !(wordsvalidated.includes('THE TERRACOTTA ARMY'))) {
-        $('#result').text("Terracotta, you found it!");
+        $('#result').text("You found it!");
         timeEvent()
         const count = wordsvalidated.push('THE TERRACOTTA ARMY')
         console.log(count)
@@ -98,35 +99,35 @@ $('#formgarage').on('submit', function(event) {
         console.log(wordsvalidated)
         countscreen();
     } else if (words === "DALI" && !(wordsvalidated.includes('DALI'))) {
-        $('#result').text("Good answer!");
+        $('#result').text("Dali, you found it!");
         timeEvent()
         const count = wordsvalidated.push('DALI')
         console.log(count)
         console.log(wordsvalidated)
         countscreen();
     } else if (words === "TOUTANKHAMON" && !(wordsvalidated.includes('TOUTANKHAMON'))) {
-        $('#result').text("Good answer!");
+        $('#result').text("you found it!");
         timeEvent()
         const count = wordsvalidated.push('TOUTANKHAMON')
         console.log(count)
         console.log(wordsvalidated)
         countscreen();
     } else if (words === "POP MASTERS" && !(wordsvalidated.includes('POP MASTERS'))) {
-        $('#result').text("Good answer!");
+        $('#result').text("You found it!");
         timeEvent()
         const count = wordsvalidated.push('POP MASTERS')
         console.log(count)
         console.log(wordsvalidated)
         countscreen();
     } else if (words === "INCA" && !(wordsvalidated.includes('INCA'))) {
-        $('#result').text("Good answer!");
+        $('#result').text("You found it!");
         timeEvent()
         const count = wordsvalidated.push('INCA')
         console.log(count)
         console.log(wordsvalidated)
         countscreen();
     } else if (words === "PHOTOCITY" && !(wordsvalidated.includes('PHOTOCITY'))) {
-        $('#result').text("Good answer!");
+        $('#result').text("You found it!");
         timeEvent()
         const count = wordsvalidated.push('PHOTOCITY')
         console.log(count)
@@ -135,7 +136,7 @@ $('#formgarage').on('submit', function(event) {
         // add enter variable -----------------------------------------------------------------------------------------------------------------------------------
         // variation inca -----------------------------------------------------------------------------------------------------------------------------------
     } else if (words === "INCAS" && !(wordsvalidated.includes('INCA'))) {
-        $('#result').text("Good answer!");
+        $('#result').text("You found it!");
         timeEvent()
         const count = wordsvalidated.push('INCA')
         console.log(count)
@@ -166,14 +167,14 @@ $('#formgarage').on('submit', function(event) {
         countscreen();
         // variation van gogh -----------------------------------------------------------------------------------------------------------------------------------
     } else if (words === "VINCENT VAN GOGH" && !(wordsvalidated.includes('VAN GOGH'))) {
-        $('#result').text("Van Gogh, you found it!");
+        $('#result').text("Vincent, you found it!");
         timeEvent()
         const count = wordsvalidated.push('VAN GOGH')
         console.log(count)
         console.log(wordsvalidated)
         countscreen();
     } else if (words === "VAN GOGH VINCENT" && !(wordsvalidated.includes('VAN GOGH'))) {
-        $('#result').text("Van Gogh, you found it!");
+        $('#result').text("Vincent, you found it!");
         timeEvent()
         const count = wordsvalidated.push('VAN GOGH')
         console.log(count)
@@ -189,7 +190,7 @@ $('#formgarage').on('submit', function(event) {
         countscreen();
         // variation dinos alive -----------------------------------------------------------------------------------------------------------------------------------
     } else if (words === "DINO" && !(wordsvalidated.includes('DINOS ALIVE'))) {
-        $('#result').text("The dinos, you found it!");
+        $('#result').text("Dinos, you found it!");
         timeEvent()
         const count = wordsvalidated.push('DINOS ALIVE')
         console.log(count)
@@ -212,7 +213,7 @@ $('#formgarage').on('submit', function(event) {
         countscreen();
         // variation terracotta -----------------------------------------------------------------------------------------------------------------------------------
     } else if (words === "TERRACOTTA" && !(wordsvalidated.includes('THE TERRACOTTA ARMY'))) {
-        $('#result').text("Terracotta, you found it!");
+        $('#result').text("You found it!");
         timeEvent()
         const count = wordsvalidated.push('THE TERRACOTTA ARMY')
         console.log(count)
@@ -236,14 +237,14 @@ $('#formgarage').on('submit', function(event) {
         countscreen();
         // variation Butterflies -----------------------------------------------------------------------------------------------------------------------------------
     } else if (words === "BUTTERFLIES" && !(wordsvalidated.includes('MOMENT WITH THE BUTTERFLIES'))) {
-        $('#result').text("Butterflies, you found it!");
+        $('#result').text("You found it!");
         timeEvent()
         const count = wordsvalidated.push('MOMENT WITH THE BUTTERFLIES')
         console.log(count)
         console.log(wordsvalidated)
         countscreen();
     } else if (words === "BUTTERFLY" && !(wordsvalidated.includes('MOMENT WITH THE BUTTERFLIES'))) {
-        $('#result').text("Butterflies, you found it!");
+        $('#result').text("You found it!");
         timeEvent()
         const count = wordsvalidated.push('MOMENT WITH THE BUTTERFLIES')
         console.log(count)
@@ -257,7 +258,7 @@ $('#formgarage').on('submit', function(event) {
         console.log(count)
         console.log(wordsvalidated)
         countscreen();
-    } else if (words === "AOTB" && !(wordsvalidated.includes('THE ART OF THE BRICK'))) {
+    } else if (words === "LEGO" && !(wordsvalidated.includes('THE ART OF THE BRICK'))) {
         $('#result').text("Lego, you found it!");
         timeEvent()
         const count = wordsvalidated.push('THE ART OF THE BRICK')
@@ -311,7 +312,7 @@ $('#formgarage').on('submit', function(event) {
         countscreen();
         // variation pop master -----------------------------------------------------------------------------------------------------------------------------------
     } else if (words === "POP ART" && !(wordsvalidated.includes('POP MASTERS'))) {
-        $('#result').text("Good answer!");
+        $('#result').text("You found it");
         timeEvent()
         const count = wordsvalidated.push('POP MASTERS')
         console.log(count)
@@ -331,3 +332,8 @@ $('#formgarage').on('submit', function(event) {
         timeEvent();
     }
 });
+
+//function eventfalse() {
+//   const audioElement = new Audio('assets/false.mp3');
+//    audioElement.play();
+//}
